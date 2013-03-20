34709d7b0e9df486a63896a8a0dcb432f1ae3687
Robert David <robert.david.public@gmail.com>
2013-03-20 17:15:49 +0100

    naive simple cuda implementation

--- src/naive.cu
+++ src/naive.cu
@@ -17,17 +17,35 @@
 
 #include "poly.h"
 
-#if defined __CUDACC__
-void calculate_cuda( void )
+__global__ void multiply( int * data_a, int * data_b, int * data_c, int size )
 {
 	int i,j;
+	int tmp;
 
-	for( i=0; i<poly_size[0]; i++ )
-	{
-		for( j=0; j<poly_size[1]; j++ )
-		{
-			poly[2][i+j] += poly[0][i]*poly[1][j];
-		}
-	}
+	i = threadIdx.x;
+	j = blockIdx.x;
+
+	tmp = data_a[i]*data_b[j];
+	atomicAdd( &data_c[i+j], tmp );
 }
-#endif
+
+extern "C" void calculate_cuda( void )
+{
+	int * data_a;
+	int * data_b;
+	int * data_c;
+	cudaMalloc( &data_a, sizeof(int)*poly_size[A] );
+	cudaMalloc( &data_b, sizeof(int)*poly_size[A] );
+	cudaMalloc( &data_c, 2*sizeof(int)*poly_size[A] );
+
+	cudaMemcpy( data_a, poly[A], sizeof(int)*poly_size[A], cudaMemcpyHostToDevice );
+	cudaMemcpy( data_b, poly[B], sizeof(int)*poly_size[A], cudaMemcpyHostToDevice );
+
+	multiply<<<poly_size[A],poly_size[A]>>>( data_a, data_b, data_c, poly_size[A] );
+
+	cudaMemcpy( poly[C], data_c, 2*sizeof(int)*poly_size[A], cudaMemcpyDeviceToHost );
+
+	cudaFree( data_a );
+	cudaFree( data_b );
+	cudaFree( data_c );
+}
