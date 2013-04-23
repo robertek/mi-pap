60dec00a648ce348c6121ad91afa1cae71ea1da5
Robert David <robert.david.public@gmail.com>
2013-04-23 16:04:00 +0200

    basic CUDA karatsuba done

--- src/karatsuba.cu
+++ src/karatsuba.cu
@@ -14,23 +14,44 @@
  * =====================================================================================
  */
 #include <stdlib.h>
+#include <stdio.h>
+
+#include <cuda_runtime.h>
 
 #include "poly.h"
+
+#define THREADS 512
+
+void cuda_init( void )
+{
+  int devID=0;
+	cudaDeviceProp deviceProp;
+
+	cudaGetDeviceProperties(&deviceProp, devID);
+	if( deviceProp.major < 1 )
+	{
+		fprintf(stderr, "gpuDeviceInit(): GPU device does not support CUDA.\n");
+		exit(-1);
+	}
+	cudaSetDevice(devID);
+	printf("gpuDeviceInit() CUDA Device [%d]: \"%s\n", devID, deviceProp.name);
+}
 
 /*
  * Di coefficient
  * Di = Ai * Bi
  */
 __global__ void calculate_D(
-		unsigned long int * data_a,
-		unsigned long int * data_b,
-		unsigned long int * data_d,
-		unsigned long int size )
+		ul_int * data_a,
+		ul_int * data_b,
+		ul_int * data_d,
+		ul_int size )
 {
-	unsigned long int i;
-	for ( i=0 ; i<size ; i++ ) 
+	ul_int pos = (blockIdx.x)*blockDim.x + (threadIdx.x);
+	
+	if( pos<size )
 	{
-		data_d[i] = data_a[i]*data_b[i];
+		data_d[pos] = data_a[pos]*data_b[pos];
 	}
 }
 
@@ -55,58 +76,72 @@ __global__ void calculate_D(
  * sum = alias for "i"
  */
 __global__ void calculate_C( 
-		unsigned long int * data_a,
-		unsigned long int * data_b,
-		unsigned long int * data_c,
-		unsigned long int * data_d,
-		unsigned long int size )
+		ul_int * data_a,
+		ul_int * data_b,
+		ul_int * data_c,
+		ul_int * data_d,
+		ul_int size )
 {
-	unsigned long int i,pos,num,sum;
-	for ( sum=1 ; sum<2*(size-1) ; sum++ ) 
+	ul_int i,pos,num,sum;
+
+	sum = (blockIdx.x)*blockDim.x + (threadIdx.x);
+
+	data_c[sum] = 0;
+
+	if(sum == 0)
 	{
-		num = (sum+1) >> 1;
-		data_c[sum] = 0;
-
-		if( sum < size ) pos=0;
-		else pos = sum - size + 1;
-
-		for( i=pos ; i<num ; i++ )
-		{
-			data_c[sum] += (data_a[i] + data_a[sum-i]) * (data_b[i] + data_b[sum-i]);
-			data_c[sum] -= data_d[i];
-			data_c[sum] -= data_d[sum-i];
-		}
-
-		if( sum%2 == 0 )
-		{
-			data_c[sum] += data_d[sum/2];
-		}
+		data_c[0]=data_d[0];
+		return;
+	}
+	if(sum == 2*(size-1))
+	{
+		data_c[2*(size-1)]=data_d[size-1];
+		return;
 	}
 
-	/* set first and last number */
-	data_c[0]=data_d[0];
-	data_c[2*(size-1)]=data_d[size-1];
+	if( sum%2 == 0 )
+	{
+		data_c[sum] += data_d[sum/2];
+	}
+
+	num = (sum+1) >> 1;
+
+	if( sum < size ) pos=0;
+	else pos = sum - size + 1;
+
+	for( i=pos ; i<num ; i++ )
+	{
+		data_c[sum] += (data_a[i] + data_a[sum-i]) * (data_b[i] + data_b[sum-i]);
+		data_c[sum] -= data_d[i];
+		data_c[sum] -= data_d[sum-i];
+	}
 }
 
 extern "C" void calculate_cuda( void )
 {
-	unsigned long int size = poly_size[A];
-	unsigned long int * data_a;
-	unsigned long int * data_b;
-	unsigned long int * data_c;
-	unsigned long int * data_d;
+	ul_int size = poly_size[A];
+	ul_int * data_a;
+	ul_int * data_b;
+	ul_int * data_c;
+	ul_int * data_d;
+	ul_int blocks = size/THREADS;
 
-	cudaMalloc( &data_a, sizeof(long int)*size );
-	cudaMalloc( &data_b, sizeof(long int)*size );
-	cudaMalloc( &data_d, sizeof(long int)*size );
-	cudaMalloc( &data_c, 2*sizeof(long int)*size );
-	cudaMemcpy( data_a, poly[A], sizeof(long int)*size, cudaMemcpyHostToDevice );
-	cudaMemcpy( data_b, poly[B], sizeof(long int)*size, cudaMemcpyHostToDevice );
+	if( size%THREADS != 0 ) blocks++;
 
-	calculate_D<<<1,1>>>( data_a, data_b, data_d, size );
-	calculate_C<<<1,1>>>( data_a, data_b, data_c, data_d, size );
+	cuda_init();
 
-	cudaMemcpy( poly[C], data_c, 2*sizeof(long int)*size, cudaMemcpyDeviceToHost );
+	cudaMalloc( &data_a, sizeof(ul_int)*size );
+	cudaMemcpy( data_a, poly[A], sizeof(ul_int)*size, cudaMemcpyHostToDevice );
+	cudaMalloc( &data_b, sizeof(ul_int)*size );
+	cudaMemcpy( data_b, poly[B], sizeof(ul_int)*size, cudaMemcpyHostToDevice );
+	cudaMalloc( &data_d, sizeof(ul_int)*size );
+	cudaMalloc( &data_c, 2*sizeof(ul_int)*size );
+
+	calculate_D<<<blocks,THREADS>>>( data_a, data_b, data_d, size );
+
+	calculate_C<<<blocks,THREADS>>>( data_a, data_b, data_c, data_d, size );
+
+	cudaMemcpy( poly[C], data_c, 2*sizeof(ul_int)*size, cudaMemcpyDeviceToHost );
 	cudaFree( data_a );
 	cudaFree( data_b );
 	cudaFree( data_c );
