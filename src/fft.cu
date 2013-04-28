e9e84db4ebcf9ff824b7c89aea80e505da892d0c
Robert David <robert.david.public@gmail.com>
2013-04-28 23:44:40 +0200

    cleanup fft.cu

--- src/fft.cu
+++ src/fft.cu
@@ -16,19 +16,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <cufft.h>
-#include <math.h>
-#include <complex.h>
 
 #include "poly.h"
 
 #define C_SIZE (2*poly_size[0])
+#define C_SIZE_COMPLEX (C_SIZE*sizeof(cufftComplex))
+
 #define THREADS 1024
 
-__global__ void multiply_points( 
-		cufftComplex * dev_a,
-		cufftComplex * dev_b,
-		cufftComplex * dev_c,
-		ul_int size )
+/* init and print GPU */
+void cuda_init( int devID )
+{
+	cudaDeviceProp deviceProp;
+
+	cudaGetDeviceProperties( &deviceProp, devID );
+	if( deviceProp.major < 1 )
+	{
+		fprintf( stderr, "gpuDeviceInit(): GPU device does not support CUDA.\n" );
+		exit( -1 );
+	}
+	cudaSetDevice(devID);
+	fprintf( stderr, "gpuDeviceInit() CUDA Device [%d]: \"%s\n", devID, deviceProp.name );
+}
+
+/* just multiply complex numbers */
+__global__ void multiply_points( cufftComplex * dev_a, cufftComplex * dev_b, cufftComplex * dev_c, ul_int size )
 {
 	ul_int i = (blockIdx.x)*blockDim.x + (threadIdx.x);
 
@@ -39,29 +51,27 @@ __global__ void multiply_points(
 	}
 }
 
-void copy_mem_dev( 
-		cufftComplex * host_a,
-		cufftComplex * host_b,
-		cufftComplex * dev_a,
-		cufftComplex * dev_b )
+/* retype and copy poly to complex host_x and copy to GPU */
+void copy_mem_dev( cufftComplex * host_a, cufftComplex * host_b, cufftComplex * dev_a, cufftComplex * dev_b )
 {
 	ul_int i;
-	for(i=0 ; i<poly_size[0] ; i++)
+	for( i=0 ; i<poly_size[0] ; i++ )
 	{
 		host_a[i].x = (float)poly[A][i];
 		host_b[i].x = (float)poly[B][i];
 	}
 
-	cudaMemcpy(dev_a, host_a, C_SIZE*sizeof(cufftComplex), cudaMemcpyHostToDevice);
-	cudaMemcpy(dev_b, host_b, C_SIZE*sizeof(cufftComplex), cudaMemcpyHostToDevice);
+	cudaMemcpy( dev_a, host_a, C_SIZE_COMPLEX, cudaMemcpyHostToDevice );
+	cudaMemcpy( dev_b, host_b, C_SIZE_COMPLEX, cudaMemcpyHostToDevice );
 }
 
+/* copy array C from GPU and retype real part to int */
 void copy_mem_host( cufftComplex * host_c, cufftComplex * dev_c )
 {
-	cudaMemcpy(host_c, dev_c, C_SIZE*sizeof(cufftComplex), cudaMemcpyDeviceToHost);
+	cudaMemcpy( host_c, dev_c, C_SIZE_COMPLEX, cudaMemcpyDeviceToHost );
 
 	ul_int i;
-	for(i=0 ; i<C_SIZE ; i++)
+	for( i=0 ; i<C_SIZE ; i++ )
 	{
 		poly[C][i] = (unsigned int)(host_c[i].x/C_SIZE);
 	}
@@ -78,31 +88,41 @@ extern "C" void calculate_cuda( void )
 	cufftComplex * cu_host_poly_c;
 	ul_int blocks = C_SIZE/THREADS + 1;
 
-	cudaMalloc((void**)&cu_poly_a, sizeof(cufftComplex)*C_SIZE );
-	cudaMalloc((void**)&cu_poly_b, sizeof(cufftComplex)*C_SIZE );
-	cudaMalloc((void**)&cu_poly_c, sizeof(cufftComplex)*C_SIZE );
-	cu_host_poly_a = (cufftComplex*) calloc(sizeof(cufftComplex),C_SIZE );
-	cu_host_poly_b = (cufftComplex*) calloc(sizeof(cufftComplex),C_SIZE );
-	cu_host_poly_c = (cufftComplex*) calloc(sizeof(cufftComplex),C_SIZE );
+	cuda_init( 0 );
 
-	copy_mem_dev( cu_host_poly_a, cu_host_poly_b, cu_poly_a, cu_poly_b);
+	/* alloc required arrays */
+	cudaMalloc( (void**)&cu_poly_a, C_SIZE_COMPLEX );
+	cudaMalloc( (void**)&cu_poly_b, C_SIZE_COMPLEX );
+	cudaMalloc( (void**)&cu_poly_c, C_SIZE_COMPLEX );
+	cu_host_poly_a = (cufftComplex*) calloc( sizeof(cufftComplex), C_SIZE );
+	cu_host_poly_b = (cufftComplex*) calloc( sizeof(cufftComplex), C_SIZE );
+	cu_host_poly_c = (cufftComplex*) calloc( sizeof(cufftComplex), C_SIZE );
 
-	cufftPlan1d(&plan, C_SIZE, CUFFT_C2C, 1);
+	/* copy data to GPU */
+	copy_mem_dev( cu_host_poly_a, cu_host_poly_b, cu_poly_a, cu_poly_b );
 
-	cufftExecC2C(plan, cu_poly_a, cu_poly_a, CUFFT_FORWARD);
-	cufftExecC2C(plan, cu_poly_b, cu_poly_b, CUFFT_FORWARD);
+	/* set up cufft */
+	cufftPlan1d( &plan, C_SIZE, CUFFT_C2C, 1 );
 
+	/* call fft transformation on each input array */
+	cufftExecC2C( plan, cu_poly_a, cu_poly_a, CUFFT_FORWARD );
+	cufftExecC2C( plan, cu_poly_b, cu_poly_b, CUFFT_FORWARD );
+
+	/* multiply complex products of fft transformation */
 	multiply_points<<<blocks,THREADS>>>( cu_poly_a, cu_poly_b, cu_poly_c, C_SIZE );
 
-	cufftExecC2C(plan, cu_poly_c, cu_poly_c, CUFFT_INVERSE);
+	/* call inverse fft transformation on output array */
+	cufftExecC2C( plan, cu_poly_c, cu_poly_c, CUFFT_INVERSE );
 
+	/* copy data from GPU */
 	copy_mem_host( cu_host_poly_c, cu_poly_c );
 
-	cufftDestroy(plan);
-	cudaFree(cu_poly_a);
-	cudaFree(cu_poly_b);
-	cudaFree(cu_poly_c);
-	free(cu_host_poly_a);
-	free(cu_host_poly_b);
-	free(cu_host_poly_c);
+	/* free memory */
+	cufftDestroy( plan );
+	cudaFree( cu_poly_a );
+	cudaFree( cu_poly_b );
+	cudaFree( cu_poly_c );
+	free( cu_host_poly_a );
+	free( cu_host_poly_b );
+	free( cu_host_poly_c );
 }
